9868e3fc7cb3b0935864bd46ec7f40d0468b0f69
Sylvester Kpei <kpeis695@gmail.com>
2025-06-26 07:53:16 +0000

    LeetCode Easy

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+"""
+Contains Duplicate
+Solved 
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+"""
+def hasDuplicate(nums):
+        return not len(nums)==len(set(nums))
+nums1 = [1, 2, 3, 3]
+nums2=[1, 2, 3, 4]
+print(hasDuplicate(nums1))
+print(hasDuplicate(nums2))
+""
